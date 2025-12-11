2f596508132ac6f6d250d07f6f5cf1a5cba6cc91
kproyalaryan@gmail.com <user.email like :'kproyalaryan@gmail.com>
2025-12-11 11:20:22 +0530

    server commit

---
+++ server.js
@@ -0,0 +1,38 @@
+import dotenv from 'dotenv'
+dotenv.config();
+import express from 'express';
+import mongoose from './config/mongoose-connection.js';
+import UserRouter from './routes/userRouter.js';
+import OwnerRouter from './routes/ownerRouter.js';
+import ProductRouter from './routes/productRouter.js';
+import IndexRouter from './routes/index.js';
+import cookie from 'cookie-parser';
+import session from 'express-session';
+import flash from 'connect-flash';
+
+const app = express()
+const port = 3000 
+app.set('view engine', 'ejs');
+app.use(cookie());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/public',express.static('public'));
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false },
+  secret: 'keyboard cat'
+}))
+app.use(flash());
+
+
+
+app.use('/',IndexRouter)
+app.use('/User',UserRouter)
+app.use('/Owner',OwnerRouter)
+app.use('/Product',ProductRouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
